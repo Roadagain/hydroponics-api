e6ea5250e3f1a17915fa0e47094658e5315ab3a1
Roadagain <ringoh72@gmail.com>
2017-09-26 17:56:32 +0900

    サーバスクリプトに追加した各ページのルーティングを設定

--- server.py
+++ server.py
@@ -10,6 +10,18 @@ def index():
     title = 'Welcome'
     return render_template('index.html', message=message, title=title)
 
+@app.route('/images')
+def images():
+    return render_template('images.html')
+
+@app.route('/data')
+def data():
+    return render_template('data.html')
+
+@app.route('/graph')
+def graph():
+    return render_template('graph.html')
+
 if __name__ == '__main__':
     app.debug = True
     app.run(port=8000)
