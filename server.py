d7bc0d0b2651ef1a431a6322059d2a81dad33b9f
Roadagain <rigoh72@gmail.com>
2017-10-06 23:45:11 +0900

    サーバスクリプトを書き直し

---
+++ server.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+#coding: utf-8
+
+from flask import Flask, Response
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return Response(response = 'Hello', status_code = 200, content_type = 'text/plain')
+
+if __name__ == '__main__':
+    app.run()
