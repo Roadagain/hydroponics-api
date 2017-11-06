36ee5ff5ba624f623e50cfbfbec166a2f200c166
Roadagain <ringoh72@gmail.com>
2017-11-06 15:53:14 +0900

    POSTされたJSONをDBに保存

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 #coding: utf-8
 
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify, make_response, request
 from pymongo import MongoClient
+import json
 
 app = Flask(__name__)
 client = MongoClient('localhost', 27017)
@@ -12,6 +13,14 @@ co = db.test_collection
 @app.route('/')
 def hello():
     res = {'data': 'Hello'}
+    return make_response(jsonify(res))
+
+@app.route('/update/<key>', methods=['POST'])
+def update(key):
+    data = request.json
+    print(data)
+    res = {'result': True}
+    co.insert_one(data)
     return make_response(jsonify(res))
 
 def main():
